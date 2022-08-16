8ddf2fe4763ae8e25e84c25d22833c178ba793fd
nsgiunta <nsgiunta@gmail.com>
2022-08-16 12:30:45 +0200

    Initial commit

---
+++ src/main/java/co/develhope/Environments01/Environments01Application.java
@@ -0,0 +1,13 @@
+package co.develhope.Environments01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Environments01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Environments01Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/develhope/Environments01/Environments01ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.Environments01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Environments01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
